acfe7e53cffc2b55e61a35fab9503878bd628775
NadezhdaDoneva <donevanad@gmail.com>
2024-06-19 21:32:24 +0300

    added command classes

--- BankingSystem/BankingSystem/CommandRegister.cpp
+++ BankingSystem/BankingSystem/CommandRegister.cpp
@@ -1,5 +1,25 @@
 #include "CommandRegister.h"
+#include "CommandRegisterClient.h"
+#include "CommandRegisterEmployee.h"
+#include "CommandRegisterThirdParty.h"
 
 void RegisterCommand::execute(Application* app) {
-	//TO DO: after regEmployeeCommand, regClientCommand, regThirdPartyCommand
+	RegClientCommand rcc;
+	RegEmployeeCommand rec;
+	RegThirdPartyCommand rtc;
+
+	int n = 0;
+	std::cout << "Choose: \n 1 - Client, \n 2 - Employee, \n 3 - Third Party \n";
+	std::cin >> n;
+	switch (n)
+	{
+	case 1: rcc.execute(app);
+		break;
+	case 2: rec.execute(app);
+		break;
+	case 3: rtc.execute(app);
+	default:
+		std::cout << "Unknown command\n";
+		break;
+	}
 }
---
+++ BankingSystem/BankingSystem/HandleThirdPartyCommand.cpp
@@ -0,0 +1,5 @@
+#include "HandleThirdPartyCommand.h"
+
+void HandleThirdPartyCommand::execute(Application* app) {
+	//TO DO: after doing every single third party command
+}
